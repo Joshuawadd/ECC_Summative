90c70c2f6aebbc544005eb2c042c527db5001c88
Joshuawadd <joshua.j.waddington@durham.ac.uk>
2019-01-31 12:18:13 +0000

    completed question 1, need to test more

--- xgnq82.py
+++ xgnq82.py
@@ -1,5 +1,19 @@
 def message(a):
-    return []
+    l = len(a)
+    rfound = False
+    r=2
+    m=[]
+    while rfound == False:
+        k = 2**r - r - 1
+        if k-r >= l :
+            rfound = True
+        else:
+            r+=1
+    m.extend(decimalToVector(l,r))
+    m.extend(a)
+    for i in range (0, k-len(m)):
+        m.append(0)
+    return m
 
 def hammingEncoder(m):
     return []
@@ -34,3 +48,53 @@ def repetitionDecoder(v):
     else:
         m = []
     return m
+
+#function HammingG
+#input: a number r
+#output: G, the generator matrix of the (2^r-1,2^r-r-1) Hamming code
+def hammingGeneratorMatrix(r):
+    n = 2**r-1
+    
+    #construct permutation pi
+    pi = []
+    for i in range(r):
+        pi.append(2**(r-i-1))
+    for j in range(1,r):
+        for k in range(2**j+1,2**(j+1)):
+            pi.append(k)
+
+    #construct rho = pi^(-1)
+    rho = []
+    for i in range(n):
+        rho.append(pi.index(i+1))
+
+    #construct H'
+    H = []
+    for i in range(r,n):
+        H.append(decimalToVector(pi[i],r))
+
+    #construct G'
+    GG = [list(i) for i in zip(*H)]
+    for i in range(n-r):
+        GG.append(decimalToVector(2**(n-r-i-1),n-r))
+
+    #apply rho to get Gtranpose
+    G = []
+    for i in range(n):
+        G.append(GG[rho[i]])
+
+    #transpose    
+    G = [list(i) for i in zip(*G)]
+
+    return G
+
+
+#function decimalToVector
+#input: numbers n and r (0 <= n<2**r)
+#output: a string v of r bits representing n
+def decimalToVector(n,r): 
+    v = []
+    for s in range(r):
+        v.insert(0,n%2)
+        n //= 2
+    return v
